888559b7ee20dd9c6822bae82660b973e2d7a954
Luiz Henrique Beluci Terra <lbeluci@gmail.com>
2021-04-14 21:58:39 -0300

    Add project files.

---
+++ src/OK.Tech.Api/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OK.Tech.Api.Models;
+using System.Collections.Generic;
+
+namespace OK.Tech.Api.Controllers
+{
+    [ApiController]
+    [Route("products")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<ProductViewModel>> GetProducts()
+        {
+            return Ok(new List<ProductViewModel>());
+        }
+    }
+}
---
+++ src/OK.Tech.Api/Models/ProductViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OK.Tech.Api.Models
+{
+    public class ProductViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "The {0} must be supplied")]
+        [StringLength(200, ErrorMessage = "The {0} length must be between {2} and {1}", MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The {0} must be supplied")]
+        [StringLength(1000, ErrorMessage = "The {0} length must be between {2} and {1}", MinimumLength = 3)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "The {0} must be supplied")]
+        public decimal Price { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
